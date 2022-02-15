f957ca0c853050c7c06295ccf43e8777de12b5a0
Tanya Sharma <81742592+TS-aynaT@users.noreply.github.com>
2022-02-15 13:51:38 +0530

    Add files via upload

---
+++ Validation_formfinal/app.js
@@ -0,0 +1,49 @@
+//Validation Code for Inputs
+var email = document.forms['form']['email'];
+var pass = document.forms['form']['pass'];
+
+var email_err = document.getElementById('email_error');
+var pass_err = document.getElementById('pass_error');
+
+email.addEventListener('textInput',email_verify);
+pass.addEventListener('textInput',pass_verify);
+// console.log(email);
+function validate()
+{
+    if(email.value.length < 9)
+    {
+        email.style.border = "1px solid red";
+        email_err.style.display = "block";
+        email.focus();
+        return false;
+    }
+    if(pass.value.length < 5)
+    {
+        pass.style.border = "1px solid red";
+        pass_err.style.display = "block";
+        pass.focus();
+        return false;
+    }
+}
+
+function email_verify()
+{
+    if(email.value.length < 9)
+    {
+        email.style.border = "1px solid silver";
+        email_err.style.display = "none";
+        return true;
+    }
+
+}
+
+function pass_verify()
+{
+    if(pass.value.length < 9)
+    {
+        pass.style.border = "1px solid silver";
+        pass_err.style.display = "none";
+        return true;
+    }
+
+}
